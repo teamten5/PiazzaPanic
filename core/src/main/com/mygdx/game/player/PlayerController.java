f0176e89a4550adc702465e5ace0a3cefaecb6e0
LrdByr0n <117828337+LrdByr0n@users.noreply.github.com>
2023-05-03 09:56:13 +0100

    Update PlayerController.java

--- core/src/main/com/mygdx/game/player/PlayerController.java
+++ core/src/main/com/mygdx/game/player/PlayerController.java
@@ -6,6 +6,12 @@ import com.badlogic.gdx.math.Rectangle;
 import com.mygdx.game.player.Controller;
 import com.mygdx.game.player.InputData;
 
+
+/**
+Controller that handles input data
+*/
+
+
 public class PlayerController implements Controller {
 
 
