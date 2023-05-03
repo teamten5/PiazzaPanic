3ffef7e2f843e6fab07730b0745e23f13de3b6cc
LrdByr0n <117828337+LrdByr0n@users.noreply.github.com>
2023-05-03 09:42:57 +0100

    Update Controller.java

--- core/src/main/com/mygdx/game/actors/controllers/Controller.java
+++ core/src/main/com/mygdx/game/actors/controllers/Controller.java
@@ -2,6 +2,11 @@ package com.mygdx.game.actors.controllers;
 
 import com.badlogic.gdx.utils.JsonValue;
 
+/**
+Is a controller for an actor. Can be used by the controllers implementations.
+*/
+
+
 public abstract class Controller {
 
     public float x = 0;
