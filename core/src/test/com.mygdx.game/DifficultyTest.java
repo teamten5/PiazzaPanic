bf0bf3b0649921a2e58759b28329c531c1c17bf0
LrdByr0n <117828337+LrdByr0n@users.noreply.github.com>
2023-05-03 11:13:06 +0100

    Update DifficultyTest.java

--- core/src/test/com.mygdx.game/DifficultyTest.java
+++ core/src/test/com.mygdx.game/DifficultyTest.java
@@ -1,4 +1,4 @@
-package com.mygdx.game;
+/**package com.mygdx.game;
 import static com.mygdx.game.actors.Customer.State.ENTERING;
 import static com.mygdx.game.actors.Customer.State.*;
 import static org.junit.jupiter.api.Assertions.*;
